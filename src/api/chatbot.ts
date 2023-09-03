2dacf67197805f34d263e3eabf9861b0e08f8e09
TiagoSansao <tiagossansao@gmail.com>
2023-09-03 15:14:24 -0300

    refactor(api): add encapsulation

--- src/api/chatbot.ts
+++ src/api/chatbot.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import qrcode from 'qrcode-terminal';
 import { openai } from '@/loaders/openai';
-import { Client } from 'whatsapp-web.js';
+import { Client, Message } from 'whatsapp-web.js';
 import { logger } from '@/loaders/logger';
 import ChatBotService from '@/services/chatbot';
 import { ApplicationError } from '@/errors/application';
@@ -48,36 +48,30 @@ class ChatBotAPI {
     });
   }
 
+  private async handleMessages(msg: Message) {
+    // If message has authors, it was sent on a group chat
+    if (msg.author || !msg.body.startsWith('/ai')) return;
+
+    logger.info(`Received message: ${msg.body} from ${msg.from}`);
+
+    try {
+      const ai = new AI(openai);
+      const apiURL = process.env.OCR_API_URL;
+      const apiKey = process.env.OCR_API_KEY;
+      const ocr = new OCR(apiURL, apiKey);
+      const chatBotService = new ChatBotService(ai, ocr);
+      const response = await chatBotService.execute(msg);
+
+      this.client.sendMessage(msg.from, response);
+    } catch (error: unknown) {
+      const errMsgDestination = msg.from;
+
+      this.chatErrorHandler(error, errMsgDestination);
+    }
+  }
+
   public setupMsgListener(): void {
-    this.client.on('message_create', async (msg) => {
-      // TESTING PURPOSE BLOCK BELOW
-      // If message has authors, it was sent on a group chat
-      if (
-        msg.from === '554788303706@c.us' &&
-        !msg.body.startsWith('/ai') &&
-        msg.type === 'chat'
-      )
-        return;
-      else if (msg.author === '554788303706@c.us') {
-        logger.debug('From my self');
-      } else if (msg.author) return;
-      logger.info(`Received message: ${msg.body} from ${msg.from}`);
-
-      try {
-        const ai = new AI(openai);
-        const apiURL = process.env.OCR_API_URL;
-        const apiKey = process.env.OCR_API_KEY;
-        const ocr = new OCR(apiURL, apiKey);
-        const chatBotService = new ChatBotService(ai, ocr);
-        const response = await chatBotService.execute(msg);
-
-        this.client.sendMessage(msg.from, response);
-      } catch (error: unknown) {
-        const errMsgDestination = msg.from;
-
-        this.chatErrorHandler(error, errMsgDestination);
-      }
-    });
+    this.client.on('message_create', this.handleMessages);
   }
 
   public async start(): Promise<void> {
