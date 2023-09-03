5c5c508df2968d8003d35b56e10c9f0abdf5898e
tiagosansao <tiagossansao@gmail.com>
2023-09-03 13:07:49 -0300

    fix(AI): update AI model

--- src/lib/AI.ts
+++ src/lib/AI.ts
@@ -1,3 +1,4 @@
+import { logger } from '@/loaders/logger';
 import { OpenAIApi } from 'openai';
 
 class AI {
@@ -8,13 +9,15 @@ class AI {
   }
 
   public async query(message: string): Promise<string> {
-    const completion = await this.client.createCompletion({
+    const completion = await this.client.createChatCompletion({
       model: 'gpt-4',
-      prompt: message,
+      messages: [{ role: 'user', content: message }],
       max_tokens: 2000,
     });
 
-    const answer = completion.data.choices[0].text!;
+    const answer = completion.data.choices[0].message!.content;
+
+    logger.info(`Complete received from LLM: ${answer}`);
 
     return answer;
   }
