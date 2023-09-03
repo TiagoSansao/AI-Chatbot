56cbb73dc1e2ae5a6bcef4ffcfdb4b89cb9f085c
TiagoSansao <tiagossansao@gmail.com>
2023-09-03 16:40:33 -0300

    chore(jest): add ts-jest dependencies and configs

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
+  testEnvironment: "node",
+  preset: 'ts-jest',
+  autoMock: false,
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {prefix: '<rootDir>'})
+};
