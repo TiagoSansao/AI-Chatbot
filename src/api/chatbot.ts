a26cf0c9c8ed3336328bc4796bbce705affd8c78
Tiago Sansão <tiagossansao@gmail.como>
2023-09-03 02:43:40 -0300

    refactor(chatbot): improve architecture

--- src/api/chatbot.ts
+++ src/api/chatbot.ts
@@ -1,85 +1,12 @@
 import qrcode from 'qrcode-terminal';
+import { openai } from '@/loaders/openai';
 import { Client } from 'whatsapp-web.js';
 import { logger } from '@/loaders/logger';
 import ChatBotService from '@/services/chatbot';
 import { ApplicationError } from '@/errors/application';
 import { statusCode } from '@/types/statusCode';
-
-const mockTextMsg = {
-  _data: {
-    id: {
-      fromMe: false,
-      remote: '551321911083@c.us',
-      id: '93E83B2F44C5C44D13A8811B13E5A7A3',
-      _serialized: 'false_551321911083@c.us_93E83B2F44C5C44D13A8811B13E5A7A3',
-    },
-    body: 'amor escreva uma mensagem para alegrar o dia',
-    type: 'chat',
-    t: 1686532444,
-    notifyName: 'mariaaa',
-    from: '551321911083@c.us',
-    to: '554788303706@c.us',
-    self: 'in',
-    ack: 1,
-    isNewMsg: true,
-    star: false,
-    kicNotified: false,
-    recvFresh: true,
-    isFromTemplate: false,
-    pollInvalidated: false,
-    isSentCagPollCreation: false,
-    latestEditMsgKey: null,
-    latestEditSenderTimestampMs: null,
-    broadcast: false,
-    mentionedJidList: [],
-    groupMentions: [],
-    isVcardOverMmsDocument: false,
-    isForwarded: false,
-    hasReaction: false,
-    productHeaderImageRejected: false,
-    lastPlaybackProgress: 0,
-    isDynamicReplyButtonsMsg: false,
-    isMdHistoryMsg: false,
-    stickerSentTs: 0,
-    isAvatar: false,
-    requiresDirectConnection: false,
-    links: [],
-  },
-  mediaKey: undefined,
-  id: {
-    fromMe: false,
-    remote: '551321911083@c.us',
-    id: '93E83B2F44C5C44D13A8811B13E5A7A3',
-    _serialized: 'false_551321911083@c.us_93E83B2F44C5C44D13A8811B13E5A7A3',
-  },
-  ack: 1,
-  hasMedia: false,
-  body: 'amor escreva uma mensagem para alegrar o dia',
-  type: 'chat',
-  timestamp: 1686532444,
-  from: '551321911083@c.us',
-  to: '554788303706@c.us',
-  author: undefined,
-  deviceType: 'android',
-  isForwarded: false,
-  forwardingScore: 0,
-  isStatus: false,
-  isStarred: false,
-  broadcast: false,
-  fromMe: false,
-  hasQuotedMsg: false,
-  hasReaction: false,
-  duration: undefined,
-  location: undefined,
-  vCards: [],
-  inviteV4: undefined,
-  mentionedIds: [],
-  orderId: undefined,
-  token: undefined,
-  isGif: false,
-  isEphemeral: undefined,
-  links: [],
-};
+import { AI } from '@/lib/AI';
+import { OCR } from '@/lib/OCR';
 
 class ChatBotAPI {
   private client: Client;
@@ -119,10 +46,11 @@ class ChatBotAPI {
   }
 
   public setupMsgListener(): void {
-    this.client.on('message', async (msg) => {
+    this.client.on('message_create', async (msg) => {
       logger.info(`Received message: ${msg.body} from ${msg.from}`);
 
       const chatAlways = [
+        '554788303706@c.us',
         '554792774509@c.us',
         '554789216109@c.us',
         '554788921683@c.us',
@@ -131,15 +59,16 @@ class ChatBotAPI {
         '554797585833@c.us',
         '551321911083@c.us' /*"551321911083@c.us"*/,
       ];
-      const COMMAND_PREFIX = '/';
 
       console.log(msg);
       try {
         if (msg.body.startsWith('amor') || chatAlways.indexOf(msg.from) !== -1) {
-          const chatBotService = new ChatBotService();
+          const ai = new AI(openai);
+          const ocr = new OCR();
+          const chatBotService = new ChatBotService(ai, ocr);
           const response = await chatBotService.execute(msg);
 
-          this.client.sendMessage(msg.from, response!);
+          this.client.sendMessage(msg.from, response);
         }
       } catch (error: unknown) {
         const errMsgDestination = msg.from;
@@ -154,9 +83,6 @@ class ChatBotAPI {
     this.setupQrListener();
     this.setupMsgListener();
     await this.client.initialize();
-
-    // TEST
-    this.client.emit('message', mockTextMsg);
   }
 }
 
--- src/services/chatbot.ts
+++ src/services/chatbot.ts
@@ -1,5 +1,4 @@
 import { logger } from '@/loaders/logger';
-import { openai } from '@/loaders/openai';
 import { AI } from '@/lib/AI';
 import { OCR } from '@/lib/OCR';
 import { Message, MessageMedia } from 'whatsapp-web.js';
@@ -7,24 +6,41 @@ import { UnsupportedFormatError } from '@/errors/unsupportedFormat';
 import { MediaDownloadError } from '@/errors/mediaDownload';
 
 class ChatBotService {
+  private ai: AI;
+  private ocr: OCR;
+
+  constructor(ai: AI, ocr: OCR) {
+    this.ocr = ocr;
+    this.ai = ai;
+  }
+
+  private async handleImage(msg: Message): Promise<string> {
+    const media: MessageMedia = await msg.downloadMedia();
+    if (!media) throw new MediaDownloadError();
+
+    const ocrResponse = await this.ocr.execute(media);
+    const aiResponse = await this.ai.query(ocrResponse);
+
+    return aiResponse;
+  }
+
+  private async handleText(msg: Message): Promise<string> {
+    const aiResponse = await this.ai.query(msg.body);
+
+    return aiResponse;
+  }
+
   public async execute(msg: Message) {
-    const ai = new AI(openai);
-    const ocr = new OCR();
+    logger.info(`Chatbot service began to handle ${msg.type} message.`);
 
     let response: string;
 
     switch (msg.type) {
       case 'chat':
-        response = await ai.query(msg.body);
+        response = await this.handleText(msg);
         break;
       case 'image':
-        const media: MessageMedia = await msg.downloadMedia();
-        if (!media) throw new MediaDownloadError();
-
-        const ocrResponse = await ocr.execute(media);
-        const aiResponse = await ai.query(ocrResponse);
-
-        response = aiResponse;
+        response = await this.handleImage(msg);
         break;
       default:
         throw new UnsupportedFormatError(msg.type);
